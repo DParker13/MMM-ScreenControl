dd25d695fc2fdc05ef8f8a8b7caa1725729475df
Daniel Parker <danielparker134@gmail.com>
2023-04-09 00:09:38 -0400

    Changed argv to 0

--- gpiocontrol/config.py
+++ gpiocontrol/config.py
@@ -4,10 +4,7 @@ import json
 import sys
 import platform
 
-_platform = platform.uname()[4]
-path_to_file = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-
-CONFIG = json.loads(sys.argv[1]);
+CONFIG = json.loads(sys.argv[0]);
 
 def get(key):
     return CONFIG[key]
