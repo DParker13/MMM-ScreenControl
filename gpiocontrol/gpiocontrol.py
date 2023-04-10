3a6508348e8f1d8d65fb77b345df308a709d424b
Daniel Parker <danielparker134@gmail.com>
2023-04-10 02:20:32 -0400

    Update gpiocontrol.py
    Swapped time checks to ints instead of strings

--- gpiocontrol/gpiocontrol.py
+++ gpiocontrol/gpiocontrol.py
@@ -17,12 +17,12 @@ while(True):
     current_time = time.strftime("%H%M", t)
     
     #Turn screen on
-    if str(config.get("off_time")) > current_time > str(config.get("on_time")) and isOn is False:
+    if config.get("off_time") > int(current_time) > config.get("on_time") and isOn is False:
         toggle()
         isOn = True
 
     #Turn screen off
-    if current_time > str(config.get("off_time")) and isOn:
+    if int(current_time) > config.get("off_time") and isOn:
         toggle()
         isOn = False
     
